5318c635552aacd1f4085ba057eddefa1024029c
Christian Toledana <toledana.ian@gmail.com>
2026-08-26 15:38:52 +0800

    feat: create initial Spring Boot application structure
    - Add basic Spring Boot application with `UserAddressApplication`.
    - Configure Maven wrapper for project management with version 3.3.4.
    - Add a basic `pom.xml` with dependencies for Spring Boot starter and Spring Boot test.
    - Set up initial application properties file.
    - Include a simple context load test within `UserAddressApplicationTests`.
    - Add `.gitattributes` for consistent line endings.

---
+++ backend/user-address/src/test/java/com/christiantoledana/useraddress/UserAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christiantoledana.useraddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
