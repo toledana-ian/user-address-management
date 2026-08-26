482c7109f61b2d384194654254191b1a2059fbf6
Christian Toledana <toledana.ian@gmail.com>
2026-08-27 03:20:10 +0800

    feat: extend UserController with CRUD for users and addresses
    - Add CRUD endpoints for `UserController` to handle users:
      - Retrieve user list, specific user details, create, update, and delete a user.
    - Add CRUD endpoints for managing user addresses under `/users/{userId}/addresses`:
      - List user addresses, create, update, and delete a specific address.

--- apps/api/src/main/java/com/christiantoledana/api/user/UserController.java
+++ apps/api/src/main/java/com/christiantoledana/api/user/UserController.java
@@ -1,18 +1,56 @@
 package com.christiantoledana.api.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+    //========== CRUD for user ==========
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "List of users";
     }
 
-    @GetMapping("/users")
-    public String user() {
-        return "Hello User!";
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable Long id) {
+        return "user info of "+id;
     }
+
+    @PostMapping("/{id}")
+    public String createUser(@PathVariable Long id) {
+        return "create user "+id;
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@PathVariable Long id) {
+        return "update user "+id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        return "delete user "+id;
+    }
+
+    //========== CRUD for user's address ==========
+    @GetMapping("/{userId}/addresses")
+    public String getUserAddresses(@PathVariable Long userId) {
+        return "user "+userId+" list of addresses ";
+    }
+
+    @PostMapping("/{userId}/addresses")
+    public String createUserAddress(@PathVariable Long userId) {
+        return "create user "+userId+" address ";
+    }
+
+    @PatchMapping("/{userId}/addresses/{id}")
+    public String updateUserAddress(@PathVariable Long userId, @PathVariable Long id) {
+        return "update user "+userId+" address "+id;
+    }
+
+    @DeleteMapping("/{userId}/addresses/{id}")
+    public String deleteUserAddress(@PathVariable Long userId, @PathVariable Long id) {
+        return "delete user "+userId+" address "+id;
+    }
+
 }
 
